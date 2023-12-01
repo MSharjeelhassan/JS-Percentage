9920cff99786e57d6944cab3b3ccff7734411820
MSharjeelhassan <mohammadsharjeelhassan@gmail.com>
2023-12-02 01:51:47 +0500

    COmpleted

---
+++ app.js
@@ -0,0 +1,13 @@
+var a = +prompt("Enter Physics marks");
+var b = +prompt("Enter Maths marks");
+var c = +prompt("Enter English marks");
+var d = +prompt("Enter Urdu marks");
+var e = +prompt("Enter Chemistry marks");
+
+var f = a+b+c+d+e;
+//var num ="Total Numbers"
+var total = 500;
+alert("Total Numbers"+ " " + f);
+var perct = ((f/total)*100);
+
+alert("Percentage"+ " "+perct+" "+"%");
